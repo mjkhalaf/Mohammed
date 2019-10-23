1e193686183af529c8ea100a33281867197fb740
mjkhalaf <mohammed.j.alkhalaf-1@ou.edu>
2019-10-23 17:17:04 -0500

    InitialCommit

--- Q_1.java
+++ Q_1.java
@@ -2,7 +2,13 @@
 public class Q_1 {
 	//is this a valid java code?
 	//A: yes
+	
 		public int main() {
 			return 10;
+		}
+		
+		//can you access private non static variables from the main method?
+		public void main( String[] args) {
+			
 		}
 }
